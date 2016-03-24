91849ad896e691ba1d2701ee0a2155e4152b51bb
metalshan <go2mailshan@gmail.com>
2016-03-24 18:18:31 +0530

    handling upgrades

--- workers/component-creator.js
+++ workers/component-creator.js
@@ -1,6 +1,11 @@
 "use strict"
 
 let logger = require('../logger');
+let packageJSON = require('../package.json');
+let shell = require('shelljs');
+let fs = require('fs');
+
+let baseApplicationFolder = shell.pwd();
 
 let componentMap = {
 	"route":{
@@ -17,6 +22,27 @@ let componentMap = {
 
 class Creator {
 	work(){
+		try{
+			let vcRawInfo = require(baseApplicationFolder+"/.voidcanvas/info.json");
+			let vcRawCliVersion = +vcRawInfo.cli.minVersion.replace(/\./g,"");
+			let currentCliVersion = +packageJSON.version.replace(/\./g,"");
+			logger.log("vcRawCliVersion "+vcRawCliVersion);
+			logger.log("currentCliVersion"+currentCliVersion);
+
+			if(vcRawCliVersion > currentCliVersion){
+				logger.error("The voidcanvas cli version is unable to process the request!");
+				logger.error("Kindly run the following conad to upgrade: ");
+				logger.different("npm update -g voidcanvas");
+				return false;
+			}
+		}
+		catch(e){
+			logger.error("Seems you are not in the root folder of a void canvas node project.");
+			logger.error("Please navigate to the base folder or run the following command to scaffold the project:");
+			logger.different("vc found node");
+			return false;
+		}
+
 		let componentType = process.argv[3];
 		if(!componentType || !componentMap[componentType]){
 			logger.error("You haven't specify the last argument which denotes which type of app you wanna scaffold");
