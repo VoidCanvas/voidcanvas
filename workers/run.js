3d2d9144d3a4c7d303e0eebf0a3840aa0e2d7574
metalshan <go2mailshan@gmail.com>
2016-04-01 18:42:53 +0530

    support of live run

--- workers/run.js
+++ workers/run.js
@@ -2,4 +2,24 @@
 
 let shell = require('shelljs');
 
-shell.exec("npm start");
+
+let logger = require('../logger');
+
+
+class Runner {
+	work(){
+		let runType = process.argv[3];
+
+		if(runType && runType.toLowerCase()==="live"){
+			let path = shell.pwd();
+			let serverFile = require(`${path}/package.json`).main;
+			shell.exec(`${__dirname}/../node_modules/nodemon/bin/nodemon.js ${serverFile}`);
+		}
+		else{
+			shell.exec("npm start");
+		}
+	}
+}
+
+module.exports = new Runner();
+
